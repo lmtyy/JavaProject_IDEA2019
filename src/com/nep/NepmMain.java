d4f4656ad3a6a3a28dd78358cf21b335c4da18a7
lmtyy <3512819960@qq.com>
2025-06-15 16:24:16 +0800

    数据库的创建以及初始化

--- src/com/nep/NepmMain.java
+++ src/com/nep/NepmMain.java
@@ -1,15 +1,18 @@
 package com.nep;
 
 import com.nep.controller.NepmLoginViewController;
+import com.nep.util.DataBasedUtil;
 import com.nep.util.JavafxUtil;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class NepmMain extends Application {
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) throws IOException, SQLException {
+        DataBasedUtil.getConnection();
         NepmLoginViewController.primaryStage = primaryStage;
         JavafxUtil.showStage(NepmMain.class,"view/NepmLoginView.fxml", primaryStage, "东软环保公众监督平台-管理端");
     }
--- src/com/nep/NepsMain.java
+++ src/com/nep/NepsMain.java
@@ -1,15 +1,18 @@
 package com.nep;
 
 import com.nep.controller.NepsLoginViewController;
+import com.nep.util.DataBasedUtil;
 import com.nep.util.JavafxUtil;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class NepsMain extends Application {
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) throws IOException, SQLException {
+        DataBasedUtil.getConnection();
         NepsLoginViewController.primaryStage = primaryStage;
         JavafxUtil.showStage(NepsMain.class,"view/NepsLoginView.fxml", primaryStage, "东软环保公众监督平台-公众监督员端");
     }
