3b62b9890b703c668f6cd9b77fe9711d0c0fc818
lmtyy <3512819960@qq.com>
2025-06-16 19:55:43 +0800

    Update DatabaseUtil.java

--- src/com/nep/util/DatabaseUtil.java
+++ src/com/nep/util/DatabaseUtil.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
-    private static final String URL="jdbc:mysql://127.0.0.1:3306/环境监督公众系统";
+    private static final String URL="jdbc:mysql://127.0.0.1:3306/nep";
     private static final String USER="root";
     private static final String PASSWORD="lmt22144";
     private static Connection connection;
