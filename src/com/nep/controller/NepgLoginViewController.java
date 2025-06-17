d830cf9608fe5084630ac543736ca2afb0d7ba33
Bobby <bibobu1223@gmail.com>
2025-06-17 14:57:57 +0800

    给网格员登录增加日志记录功能

--- src/com/nep/controller/NepgLoginViewController.java
+++ src/com/nep/controller/NepgLoginViewController.java
@@ -5,11 +5,16 @@ import com.nep.entity.GridMember;
 import com.nep.service.GridMemberService;
 import com.nep.service.impl.GridMemberServiceImpl;
 import com.nep.util.JavafxUtil;
+import com.nep.util.LogUtil;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.util.logging.Logger;
+
 public class NepgLoginViewController {
+    private static final Logger logger = LogUtil.getLogger(NepgLoginViewController.class);
+
     @FXML
     private TextField txt_id;
     @FXML
@@ -34,23 +39,24 @@ public class NepgLoginViewController {
 
     public void login(){
         if (txt_id.getText().equals("")) {
+            logger.warning(String.format("网格员登录失败: ID=%s", txt_id.getText()));
             JavafxUtil.showAlert(primaryStage, "数据格式错误", "登录账号不能为空", "请重新输入登录账号","warn");
             return;
         }
         if (txt_password.getText().equals("")) {
+            logger.warning(String.format("网格员登录失败: ID=%s", txt_id.getText()));
             JavafxUtil.showAlert(primaryStage, "数据格式错误", "登录密码不能为空", "请重新输入登录密码","warn");
             return;
         }
         NepgAqiConfirmViewController.primaryStage = primaryStage;
         GridMember gm = gridMemberService.login(txt_id.getText(), txt_password.getText());
         if (gm!=null) {
+            logger.info(String.format("网格员登录成功: ID=%s", txt_id.getText()));
             NepgAqiConfirmViewController.gridMember = gm;
             JavafxUtil.showStage(NepgMain.class, "view/NepgAqiConfirmView.fxml", primaryStage, "东软环保公众监督平台-确认AQI反馈数据");
-
         } else {
+            logger.warning(String.format("网格员登录失败: ID=%s", txt_id.getText()));
             JavafxUtil.showAlert(primaryStage, "登录失败", "登录账号和密码错误","请重新输入账号和密码","warn");
         }
-
     }
-
 }
