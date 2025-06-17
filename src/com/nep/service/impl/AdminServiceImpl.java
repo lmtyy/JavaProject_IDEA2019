102296526b03e35f8316726205da265f1a5c1a40
Bobby <bibobu1223@gmail.com>
2025-06-17 15:13:04 +0800

    格式调整

--- src/com/nep/service/impl/AdminServiceImpl.java
+++ src/com/nep/service/impl/AdminServiceImpl.java
@@ -9,25 +9,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class AdminServiceImpl implements AdminService {
-        @Override
-        public boolean login(String logincode, String password) {
-            String sql = "SELECT COUNT(*) FROM nepm WHERE account = ? AND password = ?";  // 要执行的sql语句，查询语句
+    @Override
+    public boolean login(String logincode, String password) {
+        String sql = "SELECT COUNT(*) FROM nepm WHERE account = ? AND password = ?";  // 要执行的sql语句，查询语句
 
-            try (Connection conn = DatabaseUtil.getConnection();
-                 PreparedStatement stmt = conn.prepareStatement(sql)) {  // 获取执行sql的对象 Prepared statement
+        try (Connection conn = DatabaseUtil.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {  // 获取执行sql的对象 Prepared statement
 
-                stmt.setString(1, logincode);
-                stmt.setString(2, password);
+            stmt.setString(1, logincode);
+            stmt.setString(2, password);
 
-                try (ResultSet rs = stmt.executeQuery()) {
-                    if (rs.next()) {
-                        return rs.getInt(1) > 0;  // 处理结果
-                    }
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt(1) > 0;  // 处理结果
                 }
-            } catch (SQLException e) {
-                e.printStackTrace();
             }
-            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return false;
     }
-
+}
